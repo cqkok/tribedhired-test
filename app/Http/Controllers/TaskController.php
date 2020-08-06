c28d411a392e40883fe0deacb055299797b25c13
Kok Cho Quan <m@cqkok.com>
2020-08-07 01:07:36 +0800

    Task 1

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,18 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comment;
+use App\Post;
 
 class TaskController extends Controller
 {
-    public function task1(Request $request){
-        return [
-            "task" => '1'
-        ];
+    public function task1(){
+        $comments_data = $this->fetchAPI('/comments');
+        $comments = Comment::hydrate($comments_data);
+
+        $post_comments = $comments->countBy('postId');
+
+        $posts_data = $this->fetchAPI('/posts');
+        $posts = Post::hydrate($posts_data);
+
+        $response = $posts->map(function($item, $key) use ($post_comments){
+            return [
+                'post_id' => $item->id,
+                'post_title' => $item->title,
+                'post_body' => $item->body,
+                'total_number_of_comments' => $post_comments[$item->id]
+            ];
+        });
+
+        return $response;
     }
 
     public function task2(Request $request){
+        $comments_data = $this->fetchAPI('/comments');
+        $comments = Comment::hydrate($comments_data);
+
         return [
             "task" => '2'
         ];
+    }
+
+    private function fetchAPI($api) {
+        return json_decode(file_get_contents('https://jsonplaceholder.typicode.com'.$api), true);
     }
 }
